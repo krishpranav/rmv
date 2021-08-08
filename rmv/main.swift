73c8d28b9aafa16c4ebfb91465ee89200c4abb70
krishpranav <krisna.pranav@gmail.com>
2021-08-08 19:03:30 +0530

    rmv: main[arguments]

--- rmv/main.swift
+++ rmv/main.swift
@@ -5,7 +5,25 @@
 //  Created by Elangovan Ayyasamy on 08/08/21.
 //
 
-import Foundation
+import Cocoa
 
-print("Hello, World!")
+let currnet = NSURL.fileURL(withPath: ".")
+let parent = NSURL.fileURL(withPath: "..")
 
+do
+{
+    let args = try Arguments(ProcessInfo.processInfo.arguments)
+    
+    
+    if args.files.count == 0
+    {
+        print( args.usage() ?? "No files provided" )
+        exit( -1 )
+    }
+}
+
+catch Arguments.Error.RuntimeError(let message)
+{
+    print(message)
+    exit(-1)
+}
